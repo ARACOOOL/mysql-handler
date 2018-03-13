7f68e7fd9856477ce1bf921bebafc998cc3aceb6
ARACOOOL <tkdforever@gmail.com>
2018-03-13 13:30:57 -0400

    first commit

---
+++ src/MysqlHandler.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace Neat\Logger;
+
+use Monolog\Handler\AbstractProcessingHandler;
+use Monolog\Logger;
+use PDO;
+use PDOStatement;
+
+/**
+ * Class MySQLHandler
+ * @package Neat\Logger
+ */
+class MysqlHandler extends AbstractProcessingHandler
+{
+    /**
+     * @var bool defines whether the MySQL connection is been initialized
+     */
+    private $initialized = false;
+
+    /**
+     * @var PDO pdo object of database connection
+     */
+    protected $pdo;
+
+    /**
+     * @var PDOStatement statement to insert a new record
+     */
+    private $statement;
+
+    /**
+     * @var string the table to store the logs in
+     */
+    private $table = 'logs';
+
+    /**
+     * @var array default fields that are stored in db
+     */
+    private $defaultFields = array('id', 'channel', 'level', 'short_message', 'full_message', 'payload', 'time');
+
+    /**
+     * @var array
+     */
+    private $fields = array();
+
+    /**
+     * Constructor of this class, sets the PDO and calls parent constructor
+     *
+     * @param PDO      $pdo PDO Connector for the database
+     * @param bool     $table Table in the database to store the logs in
+     * @param array    $additionalFields Additional Context Parameters to store in database
+     * @param bool|int $level Debug level which this handler should store
+     * @param bool     $bubble
+     */
+    public function __construct(
+        PDO $pdo = null,
+        $table,
+        $additionalFields = array(),
+        $level = Logger::DEBUG,
+        $bubble = true
+    )
+    {
+        if (!is_null($pdo)) {
+            $this->pdo = $pdo;
+        }
+        $this->table = $table;
+        parent::__construct($level, $bubble);
+    }
+
+    /**
+     * Initializes this handler by creating the table if it not exists
+     */
+    private function initialize()
+    {
+        $this->pdo->exec(
+            'CREATE TABLE ' . $this->table . '
+(
+    id VARCHAR(30) PRIMARY KEY NOT NULL,
+    channel VARCHAR(60) NOT NULL,
+    level INT NOT NULL,
+    short_message VARCHAR(250) NOT NULL,
+    full_message TEXT,
+    payload TEXT,
+    time DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+CREATE INDEX logs_channel_index ON logs (channel) USING HASH;
+CREATE INDEX logs_level_index ON logs (level) USING HASH;
+CREATE INDEX logs_time_index ON logs (time) USING HASH;'
+        );
+
+        $this->initialized = true;
+    }
+
+    /**
+     * Prepare the sql statement depending on the fields that should be written to the database
+     */
+    private function prepareStatement()
+    {
+        $columns = '';
+        $fields  = '';
+        foreach ($this->fields as $key => $f) {
+            if ($f == 'id') {
+                continue;
+            }
+            if ($key == 1) {
+                $columns .= "$f";
+                $fields  .= ":$f";
+                continue;
+            }
+
+            $columns .= ", $f";
+            $fields  .= ", :$f";
+        }
+
+        $this->statement = $this->pdo->prepare(
+            'INSERT INTO `' . $this->table . '` (' . $columns . ') VALUES (' . $fields . ')'
+        );
+    }
+
+
+    /**
+     * Writes the record down to the log of the implementing handler
+     *
+     * @param  $record []
+     * @return void
+     */
+    protected function write(array $record)
+    {
+        if (!$this->initialized) {
+            $this->initialize();
+        }
+
+        /**
+         * reset $fields with default values
+         */
+        $this->fields = $this->defaultFields;
+
+        //'context' contains the array
+        $contentArray = [
+            'channel'       => $record['channel'],
+            'level'         => $record['level'],
+            'short_message' => $record['short_message'],
+            'full_message'  => $record['full_message'],
+            'payload'       => json_encode($record['short_message']),
+            'time'          => $record['datetime']->format('Y-m-d H:i:s')
+        ];
+
+        // unset array keys that are passed put not defined to be stored, to prevent sql errors
+        foreach ($contentArray as $key => $context) {
+            if (!in_array($key, $this->fields)) {
+                unset($contentArray[$key]);
+                unset($this->fields[array_search($key, $this->fields)]);
+                continue;
+            }
+
+            if ($context === null) {
+                unset($contentArray[$key]);
+                unset($this->fields[array_search($key, $this->fields)]);
+            }
+        }
+
+        $this->prepareStatement();
+
+        $this->statement->execute($contentArray);
+    }
+}
